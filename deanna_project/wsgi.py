42f778f5d3a6b0fafa5a07662577a5b73699f69d
Kane005005 <traorekanz@gamil.com>
2026-01-20 18:08:05 +0000

    hebergement

--- deanna_project/wsgi.py
+++ deanna_project/wsgi.py
@@ -1,22 +1,17 @@
 """
-WSGI config for deanna_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+Fichier WSGI pour PythonAnywhere
 """
-
 import os
 import sys
 
-# Ajoutez le chemin de votre projet au Python path
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if PROJECT_DIR not in sys.path:
-    sys.path.append(PROJECT_DIR)
+# Chemin vers votre projet
+path = '/home/Deanna0025/DN'  # Remplacez 'username' par votre nom d'utilisateur PythonAnywhere
+if path not in sys.path:
+    sys.path.insert(0, path)
 
+# Définir le module de settings
+os.environ['DJANGO_SETTINGS_MODULE'] = 'deanna_project.settings'
+
+# Importer l'application Django
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deanna_project.settings')
-
 application = get_wsgi_application()
